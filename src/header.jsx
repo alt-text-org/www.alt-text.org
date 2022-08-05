6a6b3c63ab399b3ec18426c17bf071c7f5c3b51c
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-04 23:27:52 -0700

    Add sign up page

--- src/header.jsx
+++ src/header.jsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 
 export default function AltTextOrgHeader() {
     return <div className="header-wrapper">
-        <span className={"header-contents"}>
+        <span className="header-contents">
             <a className="site-title" href="https://alt-text.org">Alt-Text.org</a>
+            <button className="std-button header-sign-up-button" onClick={() => document.location.href='/sign-up.html'}>
+                Help Fill The Library
+            </button>
         </span>
     </div>
 }
