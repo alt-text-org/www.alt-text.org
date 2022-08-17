42a6ffcb106e0ba2b68c39a7f094a5f5b165f7cc
Pavel Darii <paveldarii@yahoo.com>
2022-08-16 21:41:37 -0400

    added two routes but other routes outside of / does not get hit for some reason

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
+import { BrowserRouter, Routes as ReactRoutes, Route  } from 'react-router-dom';
+
 import Main from "./main";
+import SignUp from './sign-up'
 import AltTextOrgClient from "./alt-text-org";
+
 
 const altTextOrgClient = new AltTextOrgClient()
 
 export default class App extends React.Component {
    
     render() {
-        return <>
-           <Main altTextOrgClient={altTextOrgClient}></Main>
-        </>
+        return <BrowserRouter>
+                    <ReactRoutes>
+                        <Route
+                            exact
+                            path="/"
+                            element={<Main altTextOrgClient={altTextOrgClient}></Main>}
+                        />
+                        <Route
+                            exact
+                            path="/sign-up"
+                            element={<SignUp></SignUp>}
+                        />
+                    </ReactRoutes>
+               </BrowserRouter>
     }
 }
