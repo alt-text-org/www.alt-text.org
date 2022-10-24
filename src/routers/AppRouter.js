2bd64b92521c15bf965157f8e7f9ed224b4213d5
Hannah Kolbeck <hskolbeck@gmail.com>
2022-10-24 14:03:16 -0700

    Start tweaking style

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,11 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "../views/main";
 import SignUp from "../views/sign-up";
 
-function AppRouter(props) {
+function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+          <Route path="/www.alt-text.org/public/index.html" element={<Main />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
